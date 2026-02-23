109fdce8b53c30fb413107029febb34c03673fdb
HariPrasad <58254570+Hariprasad-Diddy@users.noreply.github.com>
2026-02-23 15:28:46 +0530

    Update array_traversal.py

--- 02-Array/01-Traversal/array_traversal.py
+++ 02-Array/01-Traversal/array_traversal.py
@@ -8,3 +8,7 @@ def print_elements_of_a_list(value : list[int]) -> None:
     """
     for item in value:
         print(item)
+
+if __name__ == "__main__":
+  list_of_number = input("Enter sequence number with space : ").split(" ")
+  _print_elements_of_a_list_in_reverse(list_of_number)
