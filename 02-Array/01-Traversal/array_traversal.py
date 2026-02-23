9ca8909f6020759c733c8246f5d546bfbff05d88
HariPrasad <58254570+Hariprasad-Diddy@users.noreply.github.com>
2026-02-23 15:25:25 +0530

    Create array_traversal.py

---
+++ 02-Array/01-Traversal/array_traversal.py
@@ -0,0 +1,10 @@
+def print_elements_of_a_list(value : list[int]) -> None:
+    """
+    This function prints the elements of a list
+    Args:
+        value (list[int]): The list to print the elements of
+    Returns:
+        None
+    """
+    for item in value:
+        print(item)
